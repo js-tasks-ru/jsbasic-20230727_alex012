3e9676772c78bee7d9211a1963fa1a99c3594473
alex <de.alex25@yandex.ru>
2023-08-07 01:22:44 +0300

    Решение задач по основам и объектам

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let s in salaries) {
+    if (typeof salaries[s] === 'number' && !isNaN(salaries[s]) && salaries[s] != Infinity && salaries[s] != -Infinity) {
+      sum += salaries[s];
+    }
+  }
+
+  return sum;
 }
